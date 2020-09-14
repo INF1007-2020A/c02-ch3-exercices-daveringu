143c5755bd7e772fbcdf07b0924af3df424b93a1
daveringu <david.ringuet@polymtl.ca>
2020-09-14 17:30:37 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -18,6 +18,7 @@ def orthogonal(v1, v2):
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+    
 	for v in values:
 		pass # La variable v contient une valeur de la liste.
 
